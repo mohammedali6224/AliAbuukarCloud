3d85d3dfad76132b8dd7839b438b644f6eafd866
AliAbuukar <Mohammedali6224@outlook.com>
2021-08-23 16:54:19 +0100

    Tests

---
+++ src/test/java/TestSelectNumber.java
@@ -0,0 +1,17 @@
+import com.company.Main;
+//import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+//public class TestSelectNumber {
+
+
+   // public class testSelectNumber {
+
+      //  @Test
+        //public void TestSelectNumber() {
+           // assertEquals("Select number (1-36)", Main.getSelectNumber());
+          //  return String("Select Number(1-36)";
+     //   }
+   // }
+//}
