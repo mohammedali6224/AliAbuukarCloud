581dada35c35def3f4d29db4d1638c58e77a0db3
mohammedali6224 <89077182+mohammedali6224@users.noreply.github.com>
2021-08-23 17:14:59 +0100

    Update TestSelectNumber.java

--- src/main/java/TestSelectNumber.java
+++ src/main/java/TestSelectNumber.java
@@ -1,17 +1,17 @@
-import com.company.Main;
-import org.junit.Test;
+//import com.company.Main;
+//import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertEquals;
 
-public class TestSelectNumber {
+//public class TestSelectNumber {
 
 
-    public class testSelectNumber {
+  //  public class testSelectNumber {
 
-        @Test
-        public void TestSelectNumber() {
+     //   @Test
+       // public void TestSelectNumber() {
            // assertEquals("Select number (1-36)", Main.getSelectNumber());
           //  return String("Select Number(1-36)";
-        }
-    }
-}
+    //    }
+   // }
+//}
